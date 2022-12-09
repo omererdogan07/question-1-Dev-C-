964dec1449a991a0dd1b0ec42a5b38fa29eaaf2c
omererdogan07 <117954339+omererdogan07@users.noreply.github.com>
2022-12-10 00:37:36 +0300

    Add files via upload

---
+++ question 1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+	int number1, number2, sum;
+	printf ("enter a number1");
+	scanf ("%d" ,&number1);
+	printf ("enter a number2");
+	scanf ("%d" ,&number2);
+	sum = number1 + number2;
+	printf ("sum : %d" ,sum);
+	return 0;
+}
